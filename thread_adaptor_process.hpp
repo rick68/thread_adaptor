6b9a4f9c76d116650655e01fe35e055c907f62fc
Rick Yang <rick68@gmail.com>
2013-11-02 00:13:31 +0800

    improve readable

--- thread_adaptor_process.hpp
+++ thread_adaptor_process.hpp
@@ -4,12 +4,12 @@
 #include "thread_adaptor.hpp"
 
 THREAD_ADAPTOR_PROCESS_BEGIN(thread_process, data)
-
+{
 	std::cout << "In thread_process ..." << std::endl;
 	std::cout << "data.buf_ = ["
 			  << data.buf_ << ']' << std::endl;
 	::usleep(1000 * 1000);
-
+}
 THREAD_ADAPTOR_PROCESS_END
 
 #endif /* THREAD_ADAPTOR_PROCESS_END */
